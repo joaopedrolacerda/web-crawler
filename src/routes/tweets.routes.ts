f3cf360df2c7c4b6b69c71d13fc1c1cf24ffb26e
joao pedro <joao.pedro_hartbgroup@prestador.globo>
2023-03-01 15:49:37 -0300

    feat: firts commit

---
+++ src/routes/tweets.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+
+import TweetController from '../controllers/TweetController';
+
+const tweetsRouter = Router();
+
+const tweetController = new TweetController();
+
+tweetsRouter.post('/', tweetController.find);
+
+export default tweetsRouter;
